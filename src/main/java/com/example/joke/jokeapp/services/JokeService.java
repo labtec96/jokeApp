920133ca79473f2ee748bf1a3ccd531951f56518
labtec96 <cezaryhan1@gmail.com>
2020-02-12 15:58:39 +0100

    adding dependencies and creating service

---
+++ src/main/java/com/example/joke/jokeapp/services/JokeService.java
@@ -0,0 +1,9 @@
+package com.example.joke.jokeapp.services;
+
+/**
+ * Created by ch on 2020-02-12
+ */
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/com/example/joke/jokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.joke.jokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ch on 2020-02-12
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}

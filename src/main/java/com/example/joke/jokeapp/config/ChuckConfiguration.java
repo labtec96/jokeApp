b436e8fe9ae0b577cf6ffe87d93b816e53288c4f
labtec96 <cezaryhan1@gmail.com>
2020-02-17 19:28:18 +0100

    Adding two ways of configuration: Beans and XML

---
+++ src/main/java/com/example/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by ch on 2020-02-17
+ */
+//@Configuration
+public class ChuckConfiguration {
+
+    //@Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return  new ChuckNorrisQuotes();
+    }
+}
